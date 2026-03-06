d46171712a9b483f1f479bfa1a2870f44e8f8393
elevahmidia-star <elevah.midia@gmail.com>
2026-03-05 21:52:38 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from 'next';
 import { Montserrat, Playfair_Display } from 'next/font/google';
+import Script from "next/script";
 import './globals.css'; // Global styles
 
 const montserrat = Montserrat({
@@ -20,7 +21,24 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="pt-BR" className={`${montserrat.variable} ${playfair.variable}`}>
-      <body className="font-sans bg-[#0a0a0a] text-white antialiased" suppressHydrationWarning>{children}</body>
+<body className="font-sans bg-[#0a0a0a] text-white antialiased" suppressHydrationWarning>
+<Script id="meta-pixel" strategy="afterInteractive">
+{`
+!function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1233989982154380');
+  fbq('track', 'PageView');
+`}
+</Script>
+  {children}
+
+</body>
     </html>
   );
 }
